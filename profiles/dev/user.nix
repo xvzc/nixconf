ddfc8930346a09686ad7789949b2f1b02923ce4c
xvzc <me@xvzc.dev>
2026-03-30 08:37:25 +0900

    fix: top-level use of config arrtibute

--- profiles/dev/user.nix
+++ profiles/dev/user.nix
@@ -12,27 +12,29 @@
     };
   };
 
-  home.sessionPath = [
-    "$HOME/.local/share/JetBrains/Toolbox/scripts"
-  ];
+  config = {
+    home.sessionPath = [
+      "$HOME/.local/share/JetBrains/Toolbox/scripts"
+    ];
 
-  xdg.configFile."assets" = {
-    source = inputs.assets;
-    recursive = true;
+    xdg.configFile."assets" = {
+      source = inputs.assets;
+      recursive = true;
+    };
+
+    home.packages = with pkgs; [
+      # cava
+      fastfetch
+      gemini-cli
+      google-chrome
+      jq
+      ripgrep
+      slack
+      # spoofdpi
+      spotify
+      tree
+      vscode
+      antigravity
+    ];
   };
-
-  home.packages = with pkgs; [
-    # cava
-    fastfetch
-    gemini-cli
-    google-chrome
-    jq
-    ripgrep
-    slack
-    # spoofdpi
-    spotify
-    tree
-    vscode
-    antigravity
-  ];
 }
